cae8315e6460c8e664908e6ddc343c1f1aed6cbd
Claudio Benghi <claudio.benghi@gmail.com>
2026-08-09 19:17:01 +0200

    preparing release of accepted PR
    Changed style for string stream generation slightly for improved legibility.,

--- ids-lib.tests/SchemaLocationFallbackTests.cs
+++ ids-lib.tests/SchemaLocationFallbackTests.cs
@@ -1,4 +1,3 @@
-using AwesomeAssertions;
 using idsLib.tests.Helpers;
 using IdsLib;
 using IdsLib.IdsSchema.IdsNodes;
@@ -21,85 +20,90 @@ namespace idsLib.tests;
 /// </summary>
 public class SchemaLocationFallbackTests : BuildingSmartRepoFiles
 {
-    public SchemaLocationFallbackTests(ITestOutputHelper outputHelper)
-    {
-        XunitOutputHelper = outputHelper;
-    }
+	public SchemaLocationFallbackTests(ITestOutputHelper outputHelper)
+	{
+		XunitOutputHelper = outputHelper;
+	}
 
-    private ITestOutputHelper XunitOutputHelper { get; }
+	private ITestOutputHelper XunitOutputHelper { get; }
 
-    private const string ValidBody =
-        "<info><title>Minimal valid IDS</title></info>" +
-        "<specifications>" +
-        "<specification name=\"Walls\" ifcVersion=\"IFC4\">" +
-        "<applicability maxOccurs=\"unbounded\"><entity><name><simpleValue>IFCWALL</simpleValue></name></entity></applicability>" +
-        "<requirements><entity><name><simpleValue>IFCWALL</simpleValue></name></entity></requirements>" +
-        "</specification>" +
-        "</specifications>";
+	private const string ValidBody = """
+		<info>
+			<title>Minimal valid IDS</title>
+		</info>
+		<specifications>
+			<specification name="Walls" ifcVersion="IFC4">
+				<applicability maxOccurs="unbounded">
+					<entity><name><simpleValue>IFCWALL</simpleValue></name></entity>
+				</applicability>
+				<requirements>
+					<entity><name><simpleValue>IFCWALL</simpleValue></name></entity>
+				</requirements>
+			</specification>
+		</specifications>
+		""";
 
-    private static Stream ToStream(string schemaLocationAttribute)
-    {
-        var xml =
-            "<ids xmlns=\"http://standards.buildingsmart.org/IDS\" " +
-            "xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" " +
-            "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" +
-            schemaLocationAttribute + ">" +
-            ValidBody +
-            "</ids>";
-        return new MemoryStream(Encoding.UTF8.GetBytes(xml));
-    }
+	private Stream PrepareStream(string schemaLocationAttribute)
+	{
+		var xml =
+			$"""
+			<ids xmlns="http://standards.buildingsmart.org/IDS" 
+				xmlns:xs="http://www.w3.org/2001/XMLSchema" 
+				xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+				{schemaLocationAttribute}
+				>
+			{ValidBody}
+			</ids>
+			""";
+		XunitOutputHelper.WriteLine(xml);
+		return new MemoryStream(Encoding.UTF8.GetBytes(xml));
+	}
 
-    [Fact]
-    public void FallsBackToDeclaredIdsVersionWhenSchemaLocationMissing()
-    {
-        using var stream = ToStream(schemaLocationAttribute: string.Empty);
-        var options = new SingleAuditOptions()
-        {
-            IdsVersion = IdsVersion.Ids1_0,
-            OmitIdsContentAudit = true,
-        };
+	[Fact]
+	public void FallsBackToDeclaredIdsVersionWhenSchemaLocationMissing()
+	{
+		using var stream = PrepareStream(schemaLocationAttribute: string.Empty);
+		var options = new SingleAuditOptions()
+		{
+			IdsVersion = IdsVersion.Ids1_0,
+			OmitIdsContentAudit = true,
+		};
+		LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.Ok, expectedWarnAndErrors: 1);
+	}
 
-        LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.Ok, expectedWarnAndErrors: 1);
-    }
+	[Fact]
+	public void FallsBackToDeclaredIdsVersionWhenSchemaLocationEmpty()
+	{
+		using var stream = PrepareStream("xsi:schemaLocation=\"\"");
+		var options = new SingleAuditOptions()
+		{
+			IdsVersion = IdsVersion.Ids1_0,
+			OmitIdsContentAudit = true,
+		};
+		LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.Ok, expectedWarnAndErrors: 1);
+	}
 
-    [Fact]
-    public void FallsBackToDeclaredIdsVersionWhenSchemaLocationEmpty()
-    {
-        using var stream = ToStream(schemaLocationAttribute: " xsi:schemaLocation=\"\"");
-        var options = new SingleAuditOptions()
-        {
-            IdsVersion = IdsVersion.Ids1_0,
-            OmitIdsContentAudit = true,
-        };
+	[Fact]
+	public void DoesNotOverrideAnExistingCorrectSchemaLocation()
+	{
+		using var stream = PrepareStream("xsi:schemaLocation=\"http://standards.buildingsmart.org/IDS http://standards.buildingsmart.org/IDS/1.0/ids.xsd\"");
+		var options = new SingleAuditOptions()
+		{
+			IdsVersion = IdsVersion.Ids1_0,
+			OmitIdsContentAudit = true,
+		};
+		LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.Ok);
+	}
 
-        LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.Ok, expectedWarnAndErrors: 1);
-    }
-
-    [Fact]
-    public void DoesNotOverrideAnExistingCorrectSchemaLocation()
-    {
-        using var stream = ToStream(
-            schemaLocationAttribute: " xsi:schemaLocation=\"http://standards.buildingsmart.org/IDS http://standards.buildingsmart.org/IDS/1.0/ids.xsd\"");
-        var options = new SingleAuditOptions()
-        {
-            IdsVersion = IdsVersion.Ids1_0,
-            OmitIdsContentAudit = true,
-        };
-
-        LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.Ok);
-    }
-
-    [Fact]
-    public void StillFailsWhenAnExistingSchemaLocationIsUnrecognised()
-    {
-        using var stream = ToStream(
-            schemaLocationAttribute: " xsi:schemaLocation=\"http://standards.buildingsmart.org/IDS http://example.com/wrong.xsd\"");
-        var options = new SingleAuditOptions()
-        {
-            IdsVersion = IdsVersion.Ids1_0,
-            OmitIdsContentAudit = true,
-        };
-
-        LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.IdsStructureError);
-    }
+	[Fact]
+	public void StillFailsWhenAnExistingSchemaLocationIsUnrecognised()
+	{
+		using var stream = PrepareStream("xsi:schemaLocation=\"http://standards.buildingsmart.org/IDS http://example.com/wrong.xsd\"");
+		var options = new SingleAuditOptions()
+		{
+			IdsVersion = IdsVersion.Ids1_0,
+			OmitIdsContentAudit = true,
+		};
+		LoggerAndAuditHelpers.AuditWithStream(stream, options, XunitOutputHelper, Audit.Status.IdsStructureError);
+	}
 }
